24f1f8ba5f7d70de1021a3e69f75eb8273e6419e
AnuchitO <anuchitop@thoughtworks.com>
2021-03-19 21:33:50 +0700

    Revert "main: detele adapter"
    This reverts commit 8ba2c57f4ffffffa0c9af673764fb27b0aea5a6d.

---
+++ adapter/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+    client := &client{}
+    mac := &mac{}
+
+    client.insertLightningConnectorIntoComputer(mac)
+
+    windowsMachine := &windows{}
+    windowsMachineAdapter := &windowsAdapter{
+        windowMachine: windowsMachine,
+    }
+
+    client.insertLightningConnectorIntoComputer(windowsMachineAdapter)
+}
+
