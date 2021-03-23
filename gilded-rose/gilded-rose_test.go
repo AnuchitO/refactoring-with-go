c9618fb6c08b56e02f22f6bd1815c8806d2f9b2b
AnuchitO <anuchitop@thoughtworks.com>
2021-03-23 16:39:15 +0700

    gilded-rose: add test for normal items

--- gilded-rose/gilded-rose_test.go
+++ gilded-rose/gilded-rose_test.go
@@ -8,28 +8,55 @@ import (
 
 func TestFixture(t *testing.T) {
 	cases := []struct {
-		name    string
 		inputs  []*Item
 		outputs []*Item
 	}{
-		{name: "#1",inputs: []*Item{{"+5 Dexterity Vest", 10, 20}}, outputs: []*Item{{"+5 Dexterity Vest", 10 - 1, 20 - 1}}},
-		{name: "#2",inputs: []*Item{{"Aged Brie", 2, 0}}, outputs: []*Item{{"Aged Brie", 2 - 1, 1}}},
-		{name: "#3",inputs: []*Item{{"Elixir of the Mongoose", 5, 7}}, outputs: []*Item{{"Elixir of the Mongoose", 5 -1, 6}}},
-		{name: "#4",inputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}},
-		{name: "#5",inputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}},
-		{name: "#6",inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 15, 1}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 15 -1, 2}}},
-		{name: "#7",inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10, 49}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10 -1, 50}}},
-		{name: "#8",inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5, 49}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5 -1, 50}}},
-		//{name: "#9",inputs: []*Item{{"Conjured Mana Cake", 3, 6}},  outputs: []*Item{{"Conjured Mana Cake", 3-1, 6-2}}}, // TODO: implement new requirement
+		{inputs: []*Item{{"+5 Dexterity Vest", 10, 20}}, outputs: []*Item{{"+5 Dexterity Vest", 10 - 1, 20 - 1}}},
+		{inputs: []*Item{{"Aged Brie", 2, 0}}, outputs: []*Item{{"Aged Brie", 2 - 1, 1}}},
+		{inputs: []*Item{{"Elixir of the Mongoose", 5, 7}}, outputs: []*Item{{"Elixir of the Mongoose", 5 -1, 6}}},
+		{inputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}},
+		{inputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}},
+		{inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 15, 1}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 15 -1, 2}}},
+		{inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10, 49}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10 -1, 50}}},
+		{inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5, 49}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5 -1, 50}}},
+		//{inputs: []*Item{{"Conjured Mana Cake", 3, 6}},  outputs: []*Item{{"Conjured Mana Cake", 3-1, 6-2}}}, // TODO: implement new requirement
 	}
 
-	for _, c := range cases {
+	for i, c := range cases {
 
 		UpdateQuality(c.inputs)
 
 		if !reflect.DeepEqual(c.inputs, c.outputs) {
-			t.Errorf("%s not match : \n%#v", c.name, pretty.Diff(c.inputs, c.outputs))
+			t.Errorf("test nubmer #%d not match : \n%#v", i, pretty.Diff(c.inputs, c.outputs))
 		}
 	}
 
+}
+
+func TestNormalItems(t *testing.T) {
+	cases := []struct {
+		inputs  []*Item
+		outputs []*Item
+	}{
+		{inputs: []*Item{{"+5 Dexterity Vest", 5 ,15}}, outputs: []*Item{{"+5 Dexterity Vest", 4 ,14}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", 4 ,14}}, outputs: []*Item{{"+5 Dexterity Vest", 3 ,13}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", 3 ,13}}, outputs: []*Item{{"+5 Dexterity Vest", 2 ,12}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", 2 ,12}}, outputs: []*Item{{"+5 Dexterity Vest", 1 ,11}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", 1 ,11}}, outputs: []*Item{{"+5 Dexterity Vest", 0 ,10}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", 0 ,10}}, outputs: []*Item{{"+5 Dexterity Vest", -1, 8}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", -1, 8}}, outputs: []*Item{{"+5 Dexterity Vest", -2, 6}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", -2, 6}}, outputs: []*Item{{"+5 Dexterity Vest", -3, 4}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", -3, 4}}, outputs: []*Item{{"+5 Dexterity Vest", -4, 2}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", -4, 2}}, outputs: []*Item{{"+5 Dexterity Vest", -5, 0}}},
+		{inputs: []*Item{{"+5 Dexterity Vest", -5, 0}}, outputs: []*Item{{"+5 Dexterity Vest", -6, 0}}},
+	}
+
+	for i, c := range cases {
+
+		UpdateQuality(c.inputs)
+
+		if !reflect.DeepEqual(c.inputs, c.outputs) {
+			t.Errorf("test nubmer #%d not match : \n%#v", i, pretty.Diff(c.inputs, c.outputs))
+		}
+	}
 }
