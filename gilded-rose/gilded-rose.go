f3e3cd69e5dc03843c15c247fd0ed91b7397034b
AnuchitO <anuchitop@thoughtworks.com>
2021-03-23 17:57:41 +0700

    gilded-rose: skip update Sulfuras items

--- gilded-rose/gilded-rose.go
+++ gilded-rose/gilded-rose.go
@@ -7,11 +7,13 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for _, item := range items {
+		if item.name == "Sulfuras, Hand of Ragnaros" {
+			continue
+		}
+
 		if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
 			if item.quality > 0 {
-				if item.name != "Sulfuras, Hand of Ragnaros" {
 					item.quality = item.quality - 1
-				}
 			}
 		} else {
 			if item.quality < 50 {
@@ -31,17 +33,13 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if item.name != "Sulfuras, Hand of Ragnaros" {
 			item.sellIn = item.sellIn - 1
-		}
 
 		if item.sellIn < 0 {
 			if item.name != "Aged Brie" {
 				if item.name != "Backstage passes to a TAFKAL80ETC concert" {
 					if item.quality > 0 {
-						if item.name != "Sulfuras, Hand of Ragnaros" {
 							item.quality = item.quality - 1
-						}
 					}
 				} else {
 					item.quality = item.quality - item.quality
