c7acb5b632ae25ece697aa3026a991fe0084de2b
AnuchitO <anuchitop@thoughtworks.com>
2021-03-23 18:28:29 +0700

    gilded-rose: reduce nested if by extract method updateQuality

--- gilded-rose/gilded-rose.go
+++ gilded-rose/gilded-rose.go
@@ -12,44 +12,46 @@ func UpdateQuality(items []*Item) {
 		}
 
 		if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-			if item.quality > 0 {
-					item.quality = item.quality - 1
-			}
+			updateQuality(item, item.quality-1)
 		} else {
-			if item.quality < 50 {
-				item.quality = item.quality + 1
-				if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-					if item.sellIn < 11 {
-						if item.quality < 50 {
-							item.quality = item.quality + 1
-						}
-					}
-					if item.sellIn < 6 {
-						if item.quality < 50 {
-							item.quality = item.quality + 1
-						}
-					}
+			updateQuality(item, item.quality+1)
+			if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+				if item.sellIn < 11 {
+					updateQuality(item, item.quality+1)
+				}
+				if item.sellIn < 6 {
+					updateQuality(item, item.quality+1)
 				}
 			}
 		}
 
-			item.sellIn = item.sellIn - 1
+		item.sellIn = item.sellIn - 1
 
 		if item.sellIn < 0 {
 			if item.name != "Aged Brie" {
 				if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-					if item.quality > 0 {
-							item.quality = item.quality - 1
-					}
+					updateQuality(item, item.quality-1)
 				} else {
-					item.quality = item.quality - item.quality
+					updateQuality(item, item.quality-item.quality)
 				}
 			} else {
-				if item.quality < 50 {
-					item.quality = item.quality + 1
-				}
+				updateQuality(item, item.quality+1)
 			}
 		}
 	}
 
+}
+
+func updateQuality(item *Item, quality int) {
+	if quality > 50 {
+		item.quality = 50
+		return
+	}
+
+	if quality < 0 {
+		item.quality = 0
+		return
+	}
+
+	item.quality = quality
 }
