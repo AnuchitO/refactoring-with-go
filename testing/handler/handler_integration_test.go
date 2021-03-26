fa56aa0a0dd5167b22e8a46c1bf11c2a930d89b5
AnuchitO <anuchitop@thoughtworks.com>
2021-03-26 23:14:41 +0700

    testing: integration test

---
+++ testing/handler/handler_integration_test.go
@@ -0,0 +1,9 @@
+// +build integration
+
+package handler
+
+import "testing"
+
+func TestIntegrationGetProductHandler(t *testing.T) {
+	t.Skip("TODO: implement integration test.")
+}
---
+++ testing/handler/handler_test.go
@@ -0,0 +1,7 @@
+package handler
+
+import "testing"
+
+func TestGetProductHandler(t *testing.T) {
+	t.Skip("TODO: implement unit test.")
+}
