9fe79b2fd3ea7157c9b09fd6cac4234e3319685d
AnuchitO <anuchit.prasertasng@gmail.com>
2022-02-25 16:44:27 +0700

    rental-store: Replace Temp with Query frequentRenterPoints

--- rental/customer.go
+++ rental/customer.go
@@ -21,14 +21,12 @@ func (c Customer) Name() string {
 	return c.name
 }
 func (c Customer) Statement() string {
-	frequentRenterPoints := 0
 	result := fmt.Sprintf("Rental Record for %v\n", c.Name())
-	frequentRenterPoints = totalFrequentRenterPoints(c)
 	for _, r := range c.rentals {
 		result += fmt.Sprintf("\t%v\t%.1f\n", r.Movie().Title(), r.getCharge())
 	}
 	result += fmt.Sprintf("Amount owed is %.1f\n", totalCharge(c))
-	result += fmt.Sprintf("You earned %v frequent renter points", frequentRenterPoints)
+	result += fmt.Sprintf("You earned %v frequent renter points", totalFrequentRenterPoints(c))
 	return result
 }
 
