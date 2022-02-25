d4aba94d0f2db06d4726b7351b999925aac2c085
AnuchitO <anuchit.prasertasng@gmail.com>
2022-02-25 13:46:38 +0700

    rental-store: use go idiom to new object: Movie

--- rental/movie.go
+++ rental/movie.go
@@ -9,11 +9,11 @@ type Movie struct {
 	_priceCode int
 }
 
-func NewMovie(title string, priceCode int) (rcvr Movie) {
-	rcvr = Movie{}
-	rcvr._title = title
-	rcvr._priceCode = priceCode
-	return
+func NewMovie(title string, priceCode int) Movie {
+	return Movie{
+		_title:     title,
+		_priceCode: priceCode,
+	}
 }
 func (rcvr Movie) GetPriceCode() int {
 	return rcvr._priceCode
