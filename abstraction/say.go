2644588a16a86640a10a08c3ab5a1a4dcdbbee02
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-06-02 19:34:45 +0700

    abstraction: Web

--- abstraction/say.go
+++ abstraction/say.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	var word string
-	fmt.Println("What do you want to say?")
-	fmt.Scan(&word)
-	fmt.Printf("Say, %s!\n", word)
+	// var word string
+	// fmt.Println("What do you want to say?")
+	// fmt.Scan(&word)
+	// fmt.Printf("Say, %s!\n", word)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "form.html")
+	})
+
+	http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		word := r.FormValue("word")
+		w.Write([]byte("Say, " + word + "!\n"))
+	})
+	http.ListenAndServe("localhost:8080", nil)
 }
