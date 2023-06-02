daad6ced4e09964c49b544cd0a748c321ca3a007
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-06-02 19:45:40 +0700

    abstraction: Web - struct WebSay

--- abstraction/say.go
+++ abstraction/say.go
@@ -3,25 +3,35 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	var word string
-	fmt.Println("What do you want to say?")
-	s := &CliSay{}
-	fmt.Scan(&word)
-	s.Saying(context.Background(), word)
+	// var word string
+	// fmt.Println("What do you want to say?")
+	// s := &CliSay{}
+	// fmt.Scan(&word)
+	// s.Saying(context.Background(), word)
 
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	http.ServeFile(w, r, "form.html")
-	// })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "form.html")
+	})
 
-	// http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
-	// 	r.ParseForm()
-	// 	word := r.FormValue("word")
-	// 	w.Write([]byte(Saying(word)))
-	// })
-	// http.ListenAndServe("localhost:8080", nil)
+	http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		word := r.FormValue("word")
+		s := &WebSay{w}
+		s.Saying(r.Context(), word)
+	})
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+type WebSay struct {
+	w http.ResponseWriter
+}
+
+func (w *WebSay) Saying(ctx context.Context, word string) {
+	w.w.Write([]byte("Say, " + word + "!"))
 }
 
 type CliSay struct{}
